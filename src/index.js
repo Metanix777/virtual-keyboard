e98792882333b6f12da8592c5ea53b8b70ee1a0a
Metanix777 <shukalo.96@gmail.com>
2023-05-01 01:10:18 +0300

    feat: add creation keyboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import keysArr from "./scripts/keysData";
+
+function createMarkup() {
+  document.body.innerHTML = `
+        <main class='main'>
+            <section class='entry-field'>
+                <textarea name="entry-field__input" id="entry-field__input" cols="30" rows="10"></textarea>
+            </section>
+            <section class='keyboard'></section>
+        </main>
+    `;
+}
+
+function createKeyboard() {
+  const keyboard = document.querySelector(".keyboard");
+  keysArr.forEach((el) => {
+    const line = document.createElement("div");
+    line.className = "keyboard__line";
+    keyboard.append(line);
+
+    el.forEach((item) => {
+      const element = document.createElement("div");
+      element.className = "keyboard__item";
+      element.innerHTML = item.eng[0];
+      line.append(element);
+    });
+  });
+}
+
+createMarkup();
+createKeyboard();
